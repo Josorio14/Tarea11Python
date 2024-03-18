7e69aaaaa609e57dd99a4c43d91df622489e472d
Josorio14 <148210970+Josorio14@users.noreply.github.com>
2024-03-18 10:25:46 +0100

    Exercicis de  python

--- Tarea11Python/Ex1.py
+++ Tarea11Python/Ex1.py
@@ -1,6 +1,6 @@
 #Ex1. Tarea 11
 def lenp(frase):
-    r= frase.split(" ")
+    r = frase.split(" ")
     l = list(map(len,r))
     print("La longitud de las palabras de la lista {} es: {}".format(frase,l))
 
